4463da44d6d930b3857b0d8b3cd286e1d4039777
catsmonster <shaked.brand@gmail.com>
2019-11-18 01:45:18 +0200

    Updates

---
+++ precache-manifest.2d9140249221b23def7edb0f262efd78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21da90b38c8294ecb6869617185ea18c",
+    "url": "/Restaurant-app/index.html"
+  },
+  {
+    "revision": "1545c4ed21f4fffa22b2",
+    "url": "/Restaurant-app/static/css/main.f32c82e8.chunk.css"
+  },
+  {
+    "revision": "5abd5593a7e66560de66",
+    "url": "/Restaurant-app/static/js/2.78206c39.chunk.js"
+  },
+  {
+    "revision": "1545c4ed21f4fffa22b2",
+    "url": "/Restaurant-app/static/js/main.e22225db.chunk.js"
+  },
+  {
+    "revision": "f79dd4cea21774eee69b",
+    "url": "/Restaurant-app/static/js/runtime-main.267616f6.js"
+  }
+]);
