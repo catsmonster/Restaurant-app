576d1939c5aa991341106c79b742b716167cf61a
thenab0303 <thenab0303@gmail.com>
2019-11-21 11:26:23 -0500

    added color to food menu

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflowY: 'scroll', maxHeight: '250px', margin: '1%' }}>
+        <div className="menuList" style={{ overflowY: 'scroll', maxHeight: '250px', margin: '1%' }}>
             {props.children}
         </div>
     );
